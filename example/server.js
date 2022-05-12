0b78c40ea73d23ed24c8c2bf9cfc5dc72e1f2024
xaridar <xaridara@gmail.com>
2022-05-12 02:14:52 -0400

    rename file, linking

--- example/server.js
+++ example/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { enableRat } = require('../rat');
+const { enableRat } = require('@xaridar/rat');
 
 const app = express();
 app.use(express.static('public'));
